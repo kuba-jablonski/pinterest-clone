67a3ab0458d5cbb04275407e3fc23dfb9e910d3d
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-11-08 23:20:43 +0100

    Add AddPin component

---
+++ src/components/AddPin.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+</style>
