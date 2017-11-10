28d51a4fb58e1bf788797c673098d9e75dbefbea
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-11-10 12:09:29 +0100

    Add basic form skeleton

--- src/components/AddPin.vue
+++ src/components/AddPin.vue
@@ -1,13 +1,53 @@
 <template>
   <div class="container">
-
+    <h2>Add a New Pin</h2>
+    <form @submit.prevent="savePin">
+      <div class="inline-input">
+        <label for="link">Link</label>
+        <input v-model="imageUrl" type="text" name="link">
+      </div>
+      <div class="inline-input">
+        <label for="title">Title</label>
+        <input v-model="imageTitle" type="text" name="title">
+      </div>
+      <button>Submit</button>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: '',
+      imageTitle: '',
+    };
+  },
+  methods: {
+    savePin() {
+      // !!!
+    },
+    verifyUrl(url) {
+      return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+    },
+  },
+};
+</script>
+
 
 <style lang="scss" scoped>
 .container {
   display: flex;
   flex-direction: column;
   align-content: center;
+}
+
+input {
+  background: #fff;
+  border: 1px solid $navbar-color;
+}
+
+button {
+  border: 1px solid $navbar-color;
 }
 </style>
