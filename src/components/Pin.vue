aceb9427a0c0ad1b1472f47cbcacfc282b860a96
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-11-13 10:41:17 +0100

    Add placeholder created and likes display

--- src/components/Pin.vue
+++ src/components/Pin.vue
@@ -2,7 +2,21 @@
   <div class="item">
     <div v-masonry-tile class="pin">
       <img :src="pin.imageUrl" alt="An Image">
-      <h2 class="title">{{ pin.imageTitle }}</h2>
+      <div class="pin-info">
+        <h2 class="title">
+          {{ pin.imageTitle }}
+        </h2>
+        <div class="bottom">
+          <div class="details created">
+            <i class="fa fa-clock-o" aria-hidden="true"></i>
+            6 min ago
+          </div>
+          <div class=" details likes">
+            <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+            6 likes
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,16 +50,39 @@ export default {
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
   img {
     width: 100%;
+    transition: filter 0.3s ease;
   }
   .title {
+    position: relative;
     font-weight: 400;
     font-size: 16px;
     text-align: center;
-    width: 100%;
+    width: 90%;
     margin: 0;
-    background: $secondary;
-    color: #fff;
+    border-bottom: 2px solid $secondary;
     padding: 5px;
   }
+  .bottom {
+    display: flex;
+    width: 90%;
+    text-align: center;
+    padding: 5px 15px 5px 0;
+    font-size: 13px;
+    color: #7e7e7e;
+    font-weight: 600;
+    .details {
+      padding: 5px;
+    }
+  }
+  &:hover img {
+    filter: grayscale(100%);
+  }
+}
+
+.actions {
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
