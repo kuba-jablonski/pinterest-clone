0f81e2f31b2f6dafe5639fe1e24843b757732052
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-11-21 20:11:35 +0100

    refactor Nav css

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,38 +1,38 @@
 <template>
   <transition name="slide" @after-leave="$redrawVueMasonry" @after-enter="$redrawVueMasonry">
-    <nav v-show="navbar">
+    <nav class="nav" v-show="navbar">
       <transition name="fade" mode="out-in">
         <div v-if="authenticated === false">
-          <div class="nav-head ">
+          <div class="nav__head ">
             Login
           </div>
-          <a @click="signInWithProvider('google.com')" class="nav-item">
+          <a @click="signInWithProvider('google.com')" class="nav__item">
             <i class="fa fa-google fa-2x icon " aria-hidden="true "></i>
             Google
           </a>
-          <a @click="signInWithProvider('github.com')" class="nav-item">
+          <a @click="signInWithProvider('github.com')" class="nav__item">
             <i class="fa fa-github fa-2x icon " aria-hidden="true "></i>
             Github
           </a>
-          <a @click="signInWithProvider('twitter.com')" class="nav-item">
+          <a @click="signInWithProvider('twitter.com')" class="nav__item">
             <i class="fa fa-twitter fa-2x icon " aria-hidden="true "></i>
             Twitter
           </a>
         </div>
         <div v-if="authenticated">
-          <a @click="toPins" class="nav-item" :class="{ active: activeTab === 'pins' }">
+          <a @click="toPins" class="nav__item" :class="{ active: activeTab === 'pins' }">
             <i class="fa fa-users fa-2x icon" aria-hidden="true"></i>
             Pins
           </a>
-          <a @click="toMyPins" class="nav-item" :class="{ active: activeTab === 'user-pins' }">
+          <a @click="toMyPins" class="nav__item" :class="{ active: activeTab === 'user-pins' }">
             <i class="fa fa-user fa-2x icon" aria-hidden="true"></i>
             My Pins
           </a>
-          <a @click="toAddPin" class="nav-item" :class="{ active: activeTab === 'add-pin' }">
+          <a @click="toAddPin" class="nav__item" :class="{ active: activeTab === 'add-pin' }">
             <i class="fa fa-plus fa-2x icon" aria-hidden="true"></i>
             Add Pin
           </a>
-          <a @click="signOut" class="nav-item">
+          <a @click="signOut" class="nav__item">
             <i class="fa fa-sign-out fa-2x icon" aria-hidden="true"></i>
             Signout
           </a>
@@ -92,43 +92,41 @@ export default {
 
 
 <style lang="scss" scoped>
-nav {
+.nav {
   position: fixed;
   top: 73px;
   width: 5rem;
   min-height: 100%;
   background: $primary;
-}
-
-.nav-head {
-  color: #fff;
-  font-weight: 600;
-  text-transform: uppercase;
-  width: 90%;
-  border-bottom: 1px solid #fff;
-  text-align: center;
-  margin: 0 auto;
-  padding-top: 1rem;
-}
-
-.nav-item {
-  display: block;
-  cursor: pointer;
-  color: #ede7f6;
-  text-transform: uppercase;
-  text-decoration: none;
-  text-align: center;
-  font-weight: 600;
-  font-size: 13px;
-  width: 100%;
-  margin: 1rem 0;
-  padding-top: 5px;
-  transition: color 0.3s ease;
-  .icon {
-    display: block;
-  }
-  &:hover {
+  &__head {
     color: #fff;
+    font-weight: 600;
+    text-transform: uppercase;
+    width: 90%;
+    border-bottom: 1px solid #fff;
+    text-align: center;
+    margin: 0 auto;
+    padding-top: 1rem;
+  }
+  &__item {
+    display: block;
+    cursor: pointer;
+    color: #ede7f6;
+    text-transform: uppercase;
+    text-decoration: none;
+    text-align: center;
+    font-weight: 600;
+    font-size: 13px;
+    width: 100%;
+    margin: 1rem 0;
+    padding-top: 5px;
+    transition: color 0.3s ease;
+    .icon {
+      display: block;
+    }
+    &:hover {
+      color: #fff;
+    }
   }
 }
 
